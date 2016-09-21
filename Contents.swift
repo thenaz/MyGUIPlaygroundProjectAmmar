e35dd257123b1713678ee517ffaa7ad8d2df63ba
thenaz <anaz7963@740s-pl-126.local>
2016-09-21 08:10:45 -0600

    Added UIKit to MyGUIplayground

---
+++ Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+import UIKit
+var DynamicView = UIView(frame: CGRectMake(100,100,100,100))
+DynamicView.backgroundColor=UIColor.blueColor()
+DynamicView.layer.cornerRadius=70
+DynamicView.layer.borderWidth=15
+
+DynamicView
+
